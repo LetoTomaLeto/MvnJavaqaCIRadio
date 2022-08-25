b47ad6792659820a7b61ea50226e1d4df28a87d1
Toma Leto <echolon@ya.ru>
2022-08-25 23:44:58 +0300

    Add changes to radio.java and radioTest.java

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -87,16 +87,4 @@ class RadioTest {
         rad.pressMinusVolume();
         assertEquals(3, rad.getCurrentVolume());
     }
-
-    @Test
-    public void testStationSum() {
-        Radio rad = new Radio(15);
-        rad.setCurrentRadioStation(5);
-        rad.pressNextStation();
-
-        int expected = 6;
-        int actual = rad.getCurrentRadioStation();
-
-        assertEquals(expected, actual);
-    }
 }
